6736ccd15c2d3dc1d5873fc89b5dd08e0e2ef31e
ikizey <ikizey@gmail.com>
2020-08-11 01:06:58 +0300

    utlize form save; save redirect information before redirect

--- urlshortener/views.py
+++ urlshortener/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import redirect, render, get_object_or_404
 
 from .models import ShortenedUrls
 from .forms import CreateShortUrlForm
-from .urlgenerator import generate_short_url
 
 
 def home(request):
@@ -17,6 +16,10 @@ def teleport(_, url):
     """Redirects to full url"""
 
     url_obj = get_object_or_404(ShortenedUrls, short_url=url)
+    url_obj.increase_redirects()
+    url_obj.set_last_redirect()
+    url_obj.save()
+
     destination_url = url_obj.full_url
     # TODO check for malicious links before redirecting
     return redirect(destination_url)
@@ -29,10 +32,7 @@ def create_url(request):
     if request.method == 'POST':
         form = CreateShortUrlForm(request.POST)
         if form.is_valid():
-            full_url = form.cleaned_data['url']
-            if not ShortenedUrls.has.full_url(full_url):
-                short_url = generate_short_url(ShortenedUrls.get_set_of.short_urls())
-                ShortenedUrls.objects.create(short_url=short_url, full_url=full_url)
+            form.save()
 
     context = {'form': form}
     return render(request, template_name, context)
