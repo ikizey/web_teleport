a89a6ffe54c7f97f938131a5fb6da0c39fb00cd8
ikizey <ikizey@gmail.com>
2020-08-10 18:41:24 +0300

    implemented home view and basic teleport (redirection)

--- urlshortener/views.py
+++ urlshortener/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
+
+from .models import ShortenedUrls
 
 
 def home(request):
     """Shows list of all stored urls"""
-    return render(request, 'urlshortener/home.html')
+    template_name = 'urlshortener/home.html'
+    urls_list = ShortenedUrls.objects.all()
+    context = {'urls': urls_list}
+    return render(request, template_name, context)
 
 
-def teleport(request, url):
+def teleport(_, url):
     """Redirects to full url"""
-    return redirect(r"http://" + url)
+
+    url_obj = get_object_or_404(ShortenedUrls, short_url=url)
+    destination_url = url_obj.full_url
+    # TODO check for malicious links before redirecting
+    return redirect(destination_url)
 
 
 def create_url(request):
